de39a170af45cba3d6b6e286b5e038cf11b13f70
Scott Norvell <scottlnorvell@gmail.com>
2013-09-23 15:33:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbcrowdComposers::Application.config.secret_token = '0191e7a608097ec6fa46a2ff34b4aa61a6f837598eda5ceb77b1a39dd4a511fecaeff09f03c09a356c8a8ac15c53dd318491f22dc5371bcd06c3bc6aca8fa049'
