d373313023fb1ac37d6a5041315eee45eb61ab54
Scott Norvell <scottlnorvell@gmail.com>
2013-09-24 15:27:29 -0400

    add soundcloud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private 
+  	def soundcloud_client(user=nil)
+  		SoundCloud.new client_id: ENV['ABCROWD_SC_CLIENT_ID'], 
+  									 client_secret: ENV['ABCROWD_SC_CLIENT_SECRET'],
+  									 access_token: user.access_token if user
+  	end
 end
