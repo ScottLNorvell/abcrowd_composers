1d04d5bee3dfc296376b9982dc83de3192d39b6d
Scott Norvell <scottlnorvell@gmail.com>
2013-09-25 17:29:11 -0400

    trucking along with lyric edits

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -13,25 +13,33 @@ class LinesController < ApplicationController
 	def create
 		@lyric_line = LyricLine.create params[:lyric_line]
 
+
 		respond_to do |format|
 			format.html { redirect_to request.referer }
-			format.js
+			format.js { render 'create' }
 		end
 	end
 
 	def next_line
+
 		current_line = LyricLine.find params[:id]
 
+		next_line = LyricLine.where("lyric_id = #{current_line.lyric_id} and order_number = #{current_line.order_number} and version_number = #{current_line.version_number + 1}").first
 
+		@lyric_line = next_line
 
 		respond_to do |format|
 			format.html { redirect_to request.referer }
-			format.js
+			format.js 
 		end
 	end
 
 	def previous_line
-		@lyric_line = LyricLine.find params[:id]
+		current_line = LyricLine.find params[:id]
+
+		previous_line = LyricLine.where("lyric_id = #{current_line.lyric_id} and order_number = #{current_line.order_number} and version_number = #{current_line.version_number - 1}").first
+
+		@lyric_line = previous_line
 
 		respond_to do |format|
 			format.html { redirect_to request.referer }
