347171f908446ae6a2db5effaf66c4f7cecb29b8
Scott Norvell <scottlnorvell@gmail.com>
2013-09-26 15:37:12 -0400

    reorganization and stuff

--- app/controllers/lyric_versions_controller.rb
+++ app/controllers/lyric_versions_controller.rb
@@ -10,10 +10,14 @@ class LyricVersionsController < ApplicationController
 	end
 
 	def create
-		lyric_version = LyricVersion.new params[:lyric_version]
+		lyric = Lyric.find params[:lyric_id] 
+		lyric_version = LyricVersion.new #params[:lyric_version]
 		lyric_version.lyric_line_ids = params[:lyric_lines]
 		lyric_version.title = "#{current_user.email.split('@')[0].capitalize}'s version"
-		lyric_version.save
+		
+		lyric.lyric_versions << lyric_version
+
+		#lyric_version.save
 		redirect_to lyric_version_path lyric_version
 	end
 
