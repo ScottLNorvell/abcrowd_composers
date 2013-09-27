77a5f9d10ebbf136d04b5e0012b4c4a3bb99519d
Scott Norvell <scottlnorvell@gmail.com>
2013-09-26 23:24:22 -0400

    cleaning layout

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -10,34 +10,44 @@ class LinesController < ApplicationController
 		dump_form lyric_line
 	end 
 
+	# User changes an existing line
 	def update
-		lyric_line = LyricLine.new params[:lyric_line]
-
-		lyric_line.version_number = get_next_version( lyric_line )
-
-		lyric_line.save
-
-		@first = params[:first]
-
-		dump_line lyric_line
+		old_lyric = LyricLine.find params[:id]
+		text = params[:lyric_line][:text]
+		p text
+		if text.length < 1 || text == old_lyric.text || !/\w/.match(text)
+			# don't save, add line back
+			@placeholder = 'please enter a line'
+			dump_line old_lyric	
+		else
+			lyric_line = LyricLine.new params[:lyric_line]
+			lyric_line.version_number = get_next_version( lyric_line )
+			lyric_line.save
+			@first = params[:first]
+			dump_line lyric_line
+		end
 	end
 
+	# User 
 	def create
-		lyric_line = LyricLine.new params[:lyric_line]
+		text = params[:lyric_line][:text]
+		if text.length < 1 || !/\w/.match(text)
+			# don't save
+			render nothing: true
+		else
+			lyric_line = LyricLine.new params[:lyric_line]
 
-		lyric_line.version_number = get_next_version( lyric_line )
+			lyric_line.version_number = get_next_version( lyric_line )
 
-		lyric_line.save
+			lyric_line.save
 
-		@first = params[:first]
+			@first = params[:first]
 
-		p params
-		# this should dump whole new line
-
-		dump_line lyric_line #, false
-
+			dump_line lyric_line 
+		end
 	end
 
+	# user changes mind and cancels the changes
 	def cancel
 		if params[:id] != 'new'
 			lyric_line = LyricLine.find params[:id]
@@ -47,26 +57,23 @@ class LinesController < ApplicationController
 		end
 	end
 
+	# user clicks [insert line] button hits this controller
 	def insert_line
 		@first = params[:placement]['first'].to_f
-
 		@second = params[:placement]['second'].to_f
-
 		@new_order_number = @first + ( @second - @first ) / 2
-
 		@lyric_id = params[:lyric_id]
-
 		@lyric_line = LyricLine.new lyric_id: @lyric_id, order_number: @new_order_number
-
 		@new = true
-
 	end
 
 	def next_line
 
 		current_line = LyricLine.find params[:id]
-
-		next_line = LyricLine.where("lyric_id = #{current_line.lyric_id} and order_number = #{current_line.order_number} and version_number = #{current_line.version_number + 1}").first
+		sql = "lyric_id = #{current_line.lyric_id} 
+					 and order_number = #{current_line.order_number} 
+					 and version_number = #{current_line.version_number + 1}"
+		next_line = LyricLine.where(sql).first
 
 		if next_line
 			dump_line next_line
@@ -77,8 +84,10 @@ class LinesController < ApplicationController
 
 	def previous_line
 		current_line = LyricLine.find params[:id]
-
-		previous_line = LyricLine.where("lyric_id = #{current_line.lyric_id} and order_number = #{current_line.order_number} and version_number = #{current_line.version_number - 1}").first
+		sql = "lyric_id = #{current_line.lyric_id} 
+					 and order_number = #{current_line.order_number} 
+					 and version_number = #{current_line.version_number - 1}"
+		previous_line = LyricLine.where(sql).first
 
 		if previous_line			
 			dump_line previous_line
@@ -90,16 +99,11 @@ class LinesController < ApplicationController
 	private
 
 		def dump_line(lyric_line)
-			if lyric_line.text.length > 2
+			@lyric_line = lyric_line
 
-				@lyric_line = lyric_line
-
-				respond_to do |format|
-					format.html { redirect_to request.referer }
-					format.js { render 'update' }
-				end
-			else
-				render nothing: true
+			respond_to do |format|
+				format.html { redirect_to request.referer }
+				format.js { render 'update' }
 			end
 		end
 
